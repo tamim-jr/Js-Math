208cbd4e651005fe5ada2a69670f4ee0d8806550
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-01 02:20:01 +0600

    Love calculator code added

--- app.js
+++ app.js
@@ -39,3 +39,20 @@ console.log(value);
 
 value = Math.max(45, 100, 2, 8);
 console.log(value);
+
+//Love calculator
+ 
+value = Math.random() * 100;
+value = Math.round(value) + 1;
+// value = Math.round(value) + 1; here we show the real number mean 100 can be show
+console.log(value);
+
+
+let firstPerson = prompt("What is your Name");
+let partnerName = prompt("what is your partner name");
+
+let lovePer = Math.random() * 100;
+lovePer = Math.floor(lovePer) + 1;
+
+alert(firstPerson + " and" + partnerName + "'s Love Percent is " + lovePer + "%");
+
