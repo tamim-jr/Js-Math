e717bbffa70020a87c67a5382713f866ec130d8c
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-01 01:19:40 +0600

    JS Math Example

---
+++ app.js
@@ -0,0 +1,30 @@
+let result;
+let num1 = 45;
+let num2 = 5;
+
+result = num1 + num2;
+result = num1 - num2;
+result = num1 * num2;
+result = num1 / num2;
+result = num1 % num2;
+
+console.log(result);
+
+
+value = Math.PI
+console.log(value);
+
+value = Math.E;
+console.log(value);
+
+value = Math.round(2.5);
+console.log(value);
+
+value = Math.round(2.4);
+console.log(value);
+
+value = Math.ceil(2.1);
+console.log(value);
+
+value = Math.floor(2.1);
+console.log(value);
